1edcc48a34192e0808187b2d3b52baf8b03e6365
Jtolen543 <jtolen543@gmail.com>
2024-08-16 23:08:06 -0400

    finished framework for tic-tac-toe

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const game_container = document.querySelector(".game-body")
+let winning_dom = []
 
 let gamelayout = Array.from({length: 3}, () =>
     Array.from({length: 3}, () =>
@@ -52,7 +53,7 @@ function Player(name, symbol, sign, wins=0, turn) {
     this.turn = turn
 }
 
-const player_1 = new Player("player1", "./media/cancel-svgrepo-com.svg", "X", 0, true)
+const player_1 = new Player("player1", "./media/cancel-svgrepo-com (1).svg", "X", 0, true)
 const player_2 = new Player("player2", "./media/circle-svgrepo-com.svg", "O", 0, false)
 let clickable = true
 
@@ -66,8 +67,9 @@ function AddClickToCells() {
                     if (addMark(determineTurn(), item)) {
                         updateBoard(bigIndex, smallIndex, determineTurn().sign)
                         let condition = checkSmallBoard(Math.floor(bigIndex / 3), bigIndex % 3)
+                        console.log(condition)
                         convertSmallToBig(condition, Math.floor(bigIndex / 3), bigIndex % 3)
-                        checkBigBoard()
+                        // checkBigBoard()
                         player_1.turn = !player_1.turn
                         player_2.turn = !player_2.turn
                         swapHighlight()
@@ -85,7 +87,6 @@ function addMark(player, cell) {
         cell.id = player.sign
         const icon = document.createElement("img")
         icon.classList.add("small-cell-img")
-        icon.classList.add("img-transition")
         icon.src = player.symbol
         cell.appendChild(icon)
         return true
@@ -109,41 +110,58 @@ function checkSmallBoard(index1, index2) {
         let grid = gamelayout[index1][index2]
         return solveTicTacToe(grid)
     }
-    else {
-        return false
-    }
+    return false
 }
 
 function checkBigBoard() {
     console.log(solveTicTacToe(gamelayout))
 }
 
+function animateWinningRow(array, game_loc) {
+    let elements = []
+    array.forEach((number) => {
+        let cell_index = number[0] * 3 + number[1] + 1
+        console.log(cell_index)
+        let element = document.querySelector(`.game-${game_loc} .cell-${cell_index}`)
+        element.id ="winning-cells"
+    })
+}
+
 function convertSmallToBig(winner, bigRow, bigCol) {
-    if (winner === "X" || winner === "O") {
-        gamelayout[bigRow][bigCol] = winner
+    if (winner === null) return
+    let dom_elements = winner.cells
+    let imagery = winner.winner
+    console.log(imagery)
+    if (imagery === "X" || imagery === "O") {
+        gamelayout[bigRow][bigCol] = imagery
         let game_location = bigRow * 3 + bigCol + 1
+        animateWinningRow(dom_elements, game_location)
         const game_dom = document.querySelector(`.game-${game_location}`)
         game_dom.innerHTML = ""
         let icon = document.createElement("img")
         icon.classList.add("big-cell-img")
-        icon.src = (player_1.sign === winner) ? player_1.symbol : player_2.symbol
+        icon.src = (player_1.sign === imagery) ? player_1.symbol : player_2.symbol
         game_dom.style.cssText = "display: flex; justify-content: center; align-items: center"
         game_dom.appendChild(icon)
     }
     else return
 }
 
+
 function solveTicTacToe(board) {
     for (let i = 0; i < 3; i++) {
         if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== "-") {
-            return board[0][i]
+            return {winner: board[0][i], cells: [[0, i], [1, i], [2, i]]}
         }
         else if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== "-") {
-            return board[0][i]
+            return {winner: board[i][0], cells: [[i, 0], [i, 1], [i, 2]]}
         }
     }
-    if ((board[0][0] === board[1][1] && board[1][1] === board[2][2] || board[0][2] === board[1][1] && board[1][1] === board[2][0]) && board[1][1] !== "-")
-        return board[1][1]
+    if ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) && board[1][1] !== "-")
+        return {winner: board[1][1], cells: [[0, 0], [1, 1], [2, 2]]}
+    else if ((board[0][2] === board[1][1] && board[1][1] === board[2][0]) && board[1][1] !== "-")
+        return {winner: board[1][1], cells: [[0, 2], [1, 1], [2, 0]]}
+    else return null
     
 }
 
